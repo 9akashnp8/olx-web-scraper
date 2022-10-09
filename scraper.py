67d005fac3cca01b53f18d37669a6d3e5ade2c4a
Akash NP <akashnp1998@gmail.com>
2022-10-09 15:40:42 +0530

    Update Scraping code block with improved element finding logic
    Improve the element search by checking for the data attributes on elements
    instead of purely relying on DOM structure.

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -47,52 +47,61 @@ while True:
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@data-aut-id="btnLoadMore"]')))
         driver.find_element(By.XPATH, '//*[@data-aut-id="btnLoadMore"]').click()
         print("Clicked 'Load More'")
-    except TimeoutException:
+    except ( NoSuchElementException, TimeoutException, StaleElementReferenceException ):
         break
 
 '''FIND LISTINGS AND STORE TO A LIST'''
 #Finding the UL element containing all the search result items (as list items)
-listings_list = driver.find_element(By.CSS_SELECTOR, "#container > main > div > div > section > div > div > div:nth-child(6) > div._3etsg > div > div:nth-child(2) > ul")
+# listings_list = driver.find_element(By.CSS_SELECTOR, "#container > main > div > div > section > div > div > div:nth-child(6) > div._3etsg > div > div:nth-child(2) > ul")
+car_listing_ul = None
+all_uls = driver.find_elements(By.TAG_NAME, 'ul')
+for uls in all_uls:
+    if uls.get_attribute("data-aut-id") == "itemsList":
+        car_listing_ul = uls
 
 #Finding All list items (li) inside the UL list
-listings = listings_list.find_elements(By.CSS_SELECTOR, "li")
+listings = car_listing_ul.find_elements(By.TAG_NAME, "li")
 
 #Initialize empty list to store scrapped data (for further writing on a csv file)
 scraped_datas = []
 
 #Loop through each list element (list item) and further find required info from their respective XPath
 for listing in listings:
-    try:
-        #Try method 1 for finding elements
-        price = listing.find_element(By.XPATH, "./a/div/span[1]").text
-        year_and_odo = listing.find_element(By.XPATH, "./a/div/span[2]").text
-        short_description = listing.find_element(By.XPATH, "./a/div/span[3]").text
-        location = listing.find_element(By.XPATH, "./a/div/div/span[1]").text
-        date_published = listing.find_element(By.XPATH, "./a/div/div/span[2]").text
-        listing_link = listing.find_element(By.XPATH, "./a")
-        ad_link = listing_link.get_attribute('href')
-        #Once data gathered, append them to the scraped_datas list
-        scraped_datas.append([price, year_and_odo, short_description, location, date_published,ad_link])
-    except NoSuchElementException:
-        #If no results being found from method 1, try method 2. If no results found in method 2, pass
+    if listing.get_attribute('data-aut-id') == 'itemBox':
         try:
-            price = listing.find_element(By.XPATH, "./a/div[1]/div[2]/span").text
-            year_and_odo = listing.find_element(By.XPATH, "./a/div[1]/div[2]/div[1]").text
-            short_description = listing.find_element(By.XPATH, "./a/div[1]/div[2]/div[2]").text
-            location = listing.find_element(By.XPATH, "./a/div[1]/div[2]/div[3]").text
-            date_published = listing.find_element(By.XPATH, "./a/div[1]/div[2]/div[3]/span").text
-            listing_link = listing.find_element(By.XPATH, "./a")
-            ad_link = listing_link.get_attribute('href')
-            scraped_datas.append([price, year_and_odo, short_description, location, date_published,ad_link])
+            ad_link = listing.find_element(By.XPATH, "./a").get_attribute("href")
         except NoSuchElementException:
-            pass
-        pass
+            print("No ad_link")
+            break
+        try:
+            ad_details_card = listing.find_element(By.CLASS_NAME, "_31uw8")
+        except NoSuchElementException:
+            print("No ad_details")
+        try:
+            ad_price = ad_details_card.find_element(By.CSS_SELECTOR, "span[data-aut-id='itemPrice']").text
+        except NoSuchElementException:
+            print("No ad_price")
+        try:
+            ad_sub_title = ad_details_card.find_element(By.CSS_SELECTOR, "div[data-aut-id='itemSubTitle']").text.split(" - ")
+            model_year = ad_sub_title[0]
+            kms_driven = ad_sub_title[1]
+        except NoSuchElementException:
+            print("No ad_sub_title")
+        try:
+            ad_title = ad_details_card.find_element(By.CSS_SELECTOR, "div[data-aut-id='itemTitle']").get_attribute('title')
+        except NoSuchElementException:
+            print("No ad_title")
+        try:
+            ad_location = ad_details_card.find_element(By.CSS_SELECTOR, "div[data-aut-id='itemDetails']").text.split("\n")[0]
+        except NoSuchElementException:
+            print("No ad_title")    
+        scraped_datas.append([ad_link, ad_price, model_year, kms_driven, ad_title, ad_location])
 
 '''WRITE SCRAPED LIST TO CSV'''
 search_query = input_url.split("/")[-1][2:] #extract the search query form the url
 with open(f'scraped_data/{search_query}.csv', 'w', newline='', encoding="utf-8") as file: 
     writer = csv.writer(file)
-    headers = ['Price','Detail','Description', 'Location', 'Date of Listing', 'Link']
+    headers = ['Ad Link','Price','Model Year', 'KMS Driven', 'Ad Title', 'Ad Location']
     writer.writerow(headers)
     for data in scraped_datas:
         writer.writerow(data)
