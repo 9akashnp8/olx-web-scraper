7eb8db511dc8f5c7feb6b4960ddfc9bafeba0200
Akash NP <akashnp1998@gmail.com>
2022-11-20 22:24:24 +0530

    Update main script to with new Mongo Upload functionality

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from lib.loader import page_source_loader
 from lib.reader import html_scraper
 from lib.writer import data_to_csv
+from lib.uploader import uploader
 
 print("OLX Web Scraper!")
 input_url = input("URL: ")
 
 page_source = page_source_loader(input_url)
 data = html_scraper(page_source=page_source)
+uploader(data)
 data_to_csv(filename='data', data=data)
 
 
