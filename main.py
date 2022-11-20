5a5729e8f43e86db0391f8a450fe2833cc1ffa8d
Akash NP <akashnp1998@gmail.com>
2022-11-20 22:27:18 +0530

    Remove CSV write functionality from main script (Optional)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from lib.loader import page_source_loader
 from lib.reader import html_scraper
-from lib.writer import data_to_csv
 from lib.uploader import uploader
 
 print("OLX Web Scraper!")
@@ -8,8 +7,11 @@ input_url = input("URL: ")
 
 page_source = page_source_loader(input_url)
 data = html_scraper(page_source=page_source)
-uploader(data)
-data_to_csv(filename='data', data=data)
+
+# This uploads the final data to mongodb, You
+# may also write the same to a csv file by making use of 
+# the data_to_csv function from the lib.writer module.
+uploader(data) 
 
 
 
